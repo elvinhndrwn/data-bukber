84afc07e9bcfc81225203636e3d5e7d92f4d3db8
elvinhndrwn <elvin.hendrawan@ist.id>
2024-03-26 10:41:50 +0700

    update payment, sorting by name, add total amount

--- controller/member_controller.js
+++ controller/member_controller.js
@@ -5,12 +5,13 @@ const req = require('express/lib/request');
 module.exports = {
     all: async (req, res) => {
         try{
-            const query = 'SELECT * FROM member';
+            const query = 'SELECT * FROM member ORDER BY name';
             const members = await db.query(query); 
             // res.json(result.rows);
 
             res.render('index', {
-                members : members.rows
+                members : members.rows,
+                total : members.rowCount * 25000
             });
 
         }catch (error) {
@@ -45,6 +46,30 @@ module.exports = {
         }catch (error) {
             console.error('Query error', error.message);
             res.status(500).send('Terjadi kesalahan saat menghapus data di database.');
+        }
+    },
+
+    update_payment: async (req, res) => {
+        try{
+            const { id } = req.params;
+            const get_query = 'SELECT * FROM member WHERE id = $1 ';
+            const members = await db.query(get_query, [id]);
+            
+            if(members.rowCount > 0){
+                const member = members.rows[0];
+                var is_paid = true;
+
+                if(member.is_paid){
+                    is_paid = false;
+                }
+
+                const update_query = 'UPDATE member SET is_paid = $1 WHERE id = $2 ';
+                await db.query(update_query, [is_paid, id]);
+            }
+            res.redirect('/')
+        }catch (error) {
+            console.error('Query error', error.message);
+            res.status(500).send('Terjadi kesalahan saat ubah data di database.');
         }
     }
 }
--- router/member_router.js
+++ router/member_router.js
@@ -9,6 +9,7 @@ const member_controller = require('../controller/member_controller')
 router.get('/', member_controller.all)
 router.post('/members', member_controller.save)
 router.get('/members/delete/:id', member_controller.delete)
+router.get('/members/update-payment/:id', member_controller.update_payment)
 
 // Export
 module.exports = router;
